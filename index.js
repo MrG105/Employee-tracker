ace6207fe257dd05b49110528a614f8dbde7c3d1
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-07-22 17:13:09 -0500

    added functions for viewing and adding departments, roles, and employees

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql');
-// const db = require('./db');
+const db = require('./db');
 
 
 // TODO:
 // Make Database (schema.sql) - DONE
 // make tables: Department (id, name), Role (id, title, salary, department_id), Employee (id, first_name, last_name, role_id, manager_id) - DONE
 // Make Seed.sql to populate tables - DONE
-// SQL Joins to combine tables in database
+// SQL Joins to combine tables in database - MAYBE?
 // inquirer prompts to populate table information
 // console.table functionality?
 // Walkthrough video
@@ -24,9 +24,9 @@ function prompts() {
                 "Add A Role",
                 "Add An Employee",
                 new inquirer.Separator(),
-                "View A Department",
-                "View A Role",
-                "View An Employee",
+                "View All Departments",
+                "View All Roles",
+                "View All Employees",
                 new inquirer.Separator(),
                 "Update Employee Role",
                 new inquirer.Separator()
@@ -43,13 +43,13 @@ function prompts() {
             case "Add An Employee":
                 addEmployee();
                 break;
-            case "View A Department":
+            case "View All Departments":
                 viewDept();
                 break;
-            case "View A Role":
+            case "View All Roles":
                 viewRole();
                 break;
-            case "View An Employee":
+            case "View All Employees":
                 viewEmployee();
                 break;
             case "Update Employee Role":
@@ -57,6 +57,132 @@ function prompts() {
                 break;
         }
     })
+}
+
+function viewDept() {
+    db.viewDepts()
+        .then(([rows]) => {
+            let departments = rows;
+            console.log("\n");
+            console.table(departments);
+        })
+        .then(() => prompts());
+}
+
+function viewRole() {
+    db.viewRoles()
+        .then(([rows]) => {
+            let roles = rows;
+            console.log("\n");
+            console.table(roles);
+        })
+        .then(() => prompts());
+}
+
+function viewEmployee() {
+    db.viewEmployees()
+        .then(([rows]) => {
+            let employees = rows;
+            console.log("\n");
+            console.table(employees);
+        })
+        .then(() => prompts());
+}
+
+function addDept() {
+    prompt([
+        {
+            name: "name",
+            message: "What Is The Name Of Your Department?"
+        }
+    ]).then(res => {
+        let name = res;
+        db.addDept(name);
+        console.log(`Added ${name.name} To The List of Departments`);
+        prompts();
+    })
+}
+
+function addEmployee() {
+    prompt([
+        {
+            name: "first_name",
+            message: "What Is Your Employee's First Name?"
+        },
+        {
+            name: "last_name",
+            message: "What Is Your Employee's Last Name?"
+        }
+    ]).then(res => {
+        let firstName = res.first_name;
+        let lastName = res.last_name;
+
+        db.viewRoles()
+        .then(([rows]) => {
+            let roles = rows;
+            const roleChoices = roles.map(({ id, title}) => ({
+                name: title,
+                value: id
+            }));
+
+            prompt([
+                {
+                type: "list",
+                name: "roleId",
+                message: "What Is Your Employee's Role?",
+                choices: roleChoices
+                }
+            ]).then (res => {
+                let roleId = res.roleId;
+                let employee = {
+                    first_name: firstName,
+                    last_name: lastName,
+                    role_id: roleId,
+                }
+
+                db.addEmployee(employee);
+                console.log(`Added ${firstName} ${lastName} To The List Of Employees`);
+                prompts();
+            })
+        }) 
+    })
+
+}
+
+function addRole() {
+    db.viewDepts()
+    .then(([rows]) => {
+        let departments = rows;
+        const deptChoices = departments.map(({id, name}) => ({
+            name: name,
+            value: id
+        }));
+        prompt([
+            {
+                name: "title",
+                message: "What is The Role?"
+            },
+            {
+                name: "salary",
+                message: "What Is The Salary?"
+            },
+            {
+                type: "list",
+                name: "department_id",
+                message: "What Department Does Your Role Work For?",
+                choices: deptChoices
+            }
+        ]).then (role => {
+            db.addRole(role);
+            console.log(`Added ${role.title} To The List Of Roles`);
+            prompts();
+        })
+    })
+
+}
+
+function updateEmployee() {
+
 }
 
 function init() {
