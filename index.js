eef55fb3069e7e93a5b9f97bff490cd3e91f36d2
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-07-22 15:43:50 -0500

    added initial prompts and switches to direct functions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql');
-const db = require('./db');
+// const db = require('./db');
 
 
 // TODO:
@@ -12,3 +12,55 @@ const db = require('./db');
 // inquirer prompts to populate table information
 // console.table functionality?
 // Walkthrough video
+
+function prompts() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "options",
+            message: "What Would You Like To Do?",
+            choices: [
+                "Add A Department",
+                "Add A Role",
+                "Add An Employee",
+                new inquirer.Separator(),
+                "View A Department",
+                "View A Role",
+                "View An Employee",
+                new inquirer.Separator(),
+                "Update Employee Role",
+                new inquirer.Separator()
+            ]                   
+        }
+    ]).then(userChoice => {
+        switch (userChoice.options) {
+            case "Add A Department":
+                addDept();
+                break;
+            case "Add A Role":
+                addRole();
+                break;
+            case "Add An Employee":
+                addEmployee();
+                break;
+            case "View A Department":
+                viewDept();
+                break;
+            case "View A Role":
+                viewRole();
+                break;
+            case "View An Employee":
+                viewEmployee();
+                break;
+            case "Update Employee Role":
+                updateEmployee();
+                break;
+        }
+    })
+}
+
+function init() {
+    prompts();
+}
+
+init();
